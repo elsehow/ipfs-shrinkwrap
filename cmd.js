5cfc3f9e10f7e62646e21acefb124d9c0700f70a
elsehow <yes@cosmopol.is>
2016-06-09 13:55:57 -0700

    now we pass /Users/t_merrn/Documents/ipfs-shrinkwrap over -p to the node script

--- cmd.js
+++ cmd.js
@@ -1,30 +1,38 @@
 #!/usr/local/bin/node
 
-// This takes an argument -p to a package.json
+// This takes an argument -p to the path where the package.json is
 // Requires that ipfs daemon is running.
 var fs = require('fs')
 var argv = require('minimist')(process.argv.slice(2))
+var curPath = argv.p
+var packJsonPath = join(curPath, 'package.json')
 var routine = argv._[0]
-var packjson = JSON.parse(fs.readFileSync(argv.p).toString())
+var packjson = JSON.parse(fs.readFileSync(packJsonPath).toString())
 if (!packjson)
   errorOut('ERR! No package.json found.')
 
+function join (p1, p2) {
+  return require('path').join(p1, p2)
+}
 
 function relative (p) {
-  return require('path').join(__dirname, p)
+  return join(__dirname, p)
 }
+
 function errorOut (msg) {
   throw new Error(msg)
   process.exit(1)
 }
+
 function printHelp () {
   fs.createReadStream(relative('help.txt')).pipe(process.stdout)
 }
+
 function shrinkwrap () {
   var execSync = require('child_process').execSync
   var shrinkHash = execSync('ipfs add -rHq node_modules | tail -n 1').toString().trim()
   packjson['ipfs-shrinkwrap'] = shrinkHash
-  fs.writeFileSync(argv.p, JSON.stringify(packjson, null, '  '))
+  fs.writeFileSync(packJsonPath, JSON.stringify(packjson, null, '  '))
   console.log('Shrinkwrapped node_modules/ ', shrinkHash)
 }
 
